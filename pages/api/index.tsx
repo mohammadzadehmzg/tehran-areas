777a2949b5113deb750b0ff6d773e7a7c7d930a0
mohammadzadehmzg <mohammadzadehmzg@gmail.com>
2023-09-14 14:19:52 +0330

    first commit

---
+++ pages/api/index.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+
+export default function Home() {
+
+    return (
+        <>
+            <header className="shadow-md">
+                <div className="container p-2 mx-auto">
+                   fsdfsdsdf
+                </div>
+
+            </header>
+            <main className="">
+
+            </main>
+        </>
+    )
+}
