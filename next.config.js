0b4a84d72b9b6c6343832dd03e0afa8d47683f3e
mohammadzadehmzg <mohammadzadehmzg@gmail.com>
2023-09-15 20:31:58 +0330

    api route

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+    basePath: '/pages'
+}
 module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import {useState, useEffect} from 'react';
 const getData = async (param: string) => {
     let hostname = location.hostname == "localhost" ? "" : hostname;
     console.log(location.hostname)
-    const res = await fetch(`https://tehran-area.vercel.app/api?name=${param}`)
+    const res = await fetch(`/api?name=${param}`)
     // The return value is *not* serialized
     // You can return Date, Map, Set, etc.
 
