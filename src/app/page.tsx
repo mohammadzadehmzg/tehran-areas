f0a24b7cbacc5cc68c0f1cbc1081884aa3e96f44
mohammadzadehmzg <mohammadzadehmzg@gmail.com>
2023-09-15 20:19:30 +0330

    api route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,11 @@ import Image from 'next/image'
 
 "use client"
 import {useState, useEffect} from 'react';
-import getConfig from 'next/config'
-const { publicRuntimeConfig } = getConfig()
+
 const getData = async (param: string) => {
     let hostname = location.hostname == "localhost" ? "" : hostname;
     console.log(location.hostname)
-    const res = await fetch(`${publicRuntimeConfig}/api?name=${param}`)
+    const res = await fetch(`https://tehran-area.vercel.app/api?name=${param}`)
     // The return value is *not* serialized
     // You can return Date, Map, Set, etc.
 
