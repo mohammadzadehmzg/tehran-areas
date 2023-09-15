617d14b83739ec7cddc9bbcfb86cc9e461459b2b
mohammadzadehmzg <mohammadzadehmzg@gmail.com>
2023-09-15 13:43:21 +0330

    api route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import {useState, useEffect} from 'react';
 
 async function getData(param: string) {
-    const res = await fetch(`http://localhost:3000/api?name=${param}`)
+    const res = await fetch(`/api?name=${param}`)
     // The return value is *not* serialized
     // You can return Date, Map, Set, etc.
 
