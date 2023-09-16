317732d2596199b3d5c73b23a386f4442475dd9e
mohammadzadehmzg <mohammadzadehmzg@gmail.com>
2023-09-16 10:37:49 +0330

    update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // basePath: '/pages'
+    serverRuntimeConfig: {
+        secret: process.env.SECRET,
+    },
+    publicRuntimeConfig: {
+        apiUrl: process.env.API_URL,
+    },
 }
+
+
 module.exports = nextConfig
